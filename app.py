a397095f7beba8b6649b728a00af927e30df9455
Ayush Noori <anoori@college.harvard.edu>
2023-10-28 14:25:03 -0400

    Add space files.

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# standard imports
+import numpy as np
+import argparse
+import itertools
+import time
+
+# write streamlit title
+st.title("Bottom-Up Enumerative Program Synthesis")
